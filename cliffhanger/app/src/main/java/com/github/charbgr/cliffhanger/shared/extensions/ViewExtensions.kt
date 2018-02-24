41c54a3402da12615fd37e523efaf92f30544f24
charbgr <bmpak04@gmail.com>
2018-02-24 19:17:51 +0200

    Add visible and gone view extensions

--- cliffhanger/app/src/main/java/com/github/charbgr/cliffhanger/shared/extensions/ViewExtensions.kt
+++ cliffhanger/app/src/main/java/com/github/charbgr/cliffhanger/shared/extensions/ViewExtensions.kt
@@ -7,3 +7,19 @@ import android.view.ViewGroup
 fun ViewGroup.render(layoutId: Int, attachToRoot: Boolean = false): View {
   return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
 }
+
+fun View.visibleOrGone(shouldBeVisible: Boolean) {
+  if (shouldBeVisible) {
+    this.visible()
+  } else {
+    this.gone()
+  }
+}
+
+fun View.visible() {
+  this.visibility = View.VISIBLE
+}
+
+fun View.gone() {
+  this.visibility = View.GONE
+}
