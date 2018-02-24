3be14802392d1093604d8cc67535907903dae4f7
charbgr <bmpak04@gmail.com>
2018-02-24 20:34:17 +0200

    Add Error screen

---
+++ cliffhanger/app/src/main/java/com/github/charbgr/cliffhanger/features/error/NavigateToError.kt
@@ -0,0 +1,10 @@
+package com.github.charbgr.cliffhanger.features.error
+
+import android.content.Context
+import android.content.Intent
+import com.github.charbgr.cliffhanger.shared.commands.AbstractActivityNavigationCommand
+
+class NavigateToError(private val context: Context) : AbstractActivityNavigationCommand() {
+  override fun getContext(): Context = context
+  override fun createIntent(): Intent = Intent(context, ErrorActivity::class.java)
+}
